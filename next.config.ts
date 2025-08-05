2e326161d059da633b40952265e6e494e83ae0c2
Mitchell <braden.mitchell@gov.bc.ca>
2025-08-05 15:08:40 -0700

    Nextjs dev container fixes

--- next.config.ts
+++ next.config.ts
@@ -7,9 +7,6 @@ const nextConfig: NextConfig = {
   // Production optimizations
   compress: true,
 
-  // Performance optimizations
-  swcMinify: true,
-
   // Security headers
   async headers() {
     return [
