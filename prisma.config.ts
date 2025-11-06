5a896d32fb4d1cf7f64a8a4ff905da5b6aefded8
BradyMitch <braden.jr.mitch@gmail.com>
2025-11-06 14:24:38 -0800

    prisma setup

---
+++ prisma.config.ts
@@ -0,0 +1,15 @@
+import "dotenv/config";
+
+import path from "node:path";
+import { defineConfig, env } from "prisma/config";
+
+export default defineConfig({
+  schema: "prisma",
+  migrations: {
+    path: path.join("prisma", "migrations"),
+  },
+  engine: "classic",
+  datasource: {
+      url: env('DATABASE_URL'),
+  }
+});
