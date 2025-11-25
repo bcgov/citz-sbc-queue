562a27c3056bb2802288af9f1e4cd251a162511f
BradyMitch <braden.jr.mitch@gmail.com>
2025-11-25 15:26:21 -0800

    admin users initial setup
    # Conflicts:
    #	src/app/protected/admin/users/page.tsx

--- src/app/protected/admin/page.tsx
+++ src/app/protected/admin/page.tsx
@@ -2,9 +2,7 @@ export default async function Page() {
   return (
     <div className="container mx-auto px-4 py-8">
       <div className="max-w-4xl mx-auto">
-        <header className="mb-8">
-          <h1 className="text-3xl font-bold text-gray-900 mb-2">Admin</h1>
-        </header>
+        <h1 className="text-3xl font-bold text-gray-900 mb-2">Admin</h1>
       </div>
     </div>
   )
