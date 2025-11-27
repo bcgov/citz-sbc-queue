97a0ce09e3c5bccd85d0228baa9f338e2e806d71
BradyMitch <braden.jr.mitch@gmail.com>
2025-11-27 10:45:41 -0800

    implemented user table

--- src/app/protected/admin/users/page.tsx
+++ src/app/protected/admin/users/page.tsx
@@ -1,11 +1,14 @@
 import { UserTable } from "@/components/admin/users/UserTable"
+import { getStaffUsers } from "@/lib/prisma/users/getStaffUsers"
 
 export default async function Page() {
+  const users = await getStaffUsers()
+
   return (
     <div className="container mx-auto px-4 py-8">
       <div className="max-w-4xl mx-auto">
         <h1 className="text-3xl font-bold text-gray-900 mb-2">Users</h1>
-        <UserTable />
+        <UserTable users={users} />
       </div>
     </div>
   )
