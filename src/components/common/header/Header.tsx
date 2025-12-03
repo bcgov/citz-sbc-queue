4af36ceb27cd2dc3eb69ce20e1b322180862c572
stoews <scott.toews@gov.bc.ca>
2025-12-03 20:03:45 +0000

    Revert "implement design system header"
    This reverts commit a2e05b471dc30123fab369ab2822a4be468943cc.

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -1,39 +1,34 @@
 "use client"
 
-// import Image from "next/image"
-// import Link from "next/link"
-// import { LoginButton, LogoutButton } from "@/components"
-// import { useAuth } from "@/hooks"
-// import styles from "./Header.module.css"
-import { Header as Dsheader } from "@bcgov/design-system-react-components"
-import { Loginout } from "@/components"
+import Image from "next/image"
+import Link from "next/link"
+import { LoginButton, LogoutButton } from "@/components"
+import { useAuth } from "@/hooks"
+import styles from "./Header.module.css"
 
 export const Header = () => {
   return (
-    <Dsheader title="SBC Queue Management">
-      <Loginout />
-    </Dsheader>
-    // <header className={styles.header}>
-    //   <div className={styles.inner}>
-    //     <div className={styles.logo}>
-    //       <Link href="/" aria-label="BC Government home">
-    //         <Image
-    //           src="/bcgov/BCID_H_RGB_pos.svg"
-    //           alt="BC Government logo"
-    //           className={styles.logoImg}
-    //           width={180}
-    //           height={56}
-    //           fetchPriority="high"
-    //         />
-    //       </Link>
-    //     </div>
+    <header className={styles.header}>
+      <div className={styles.inner}>
+        <div className={styles.logo}>
+          <Link href="/" aria-label="BC Government home">
+            <Image
+              src="/bcgov/BCID_H_RGB_pos.svg"
+              alt="BC Government logo"
+              className={styles.logoImg}
+              width={180}
+              height={56}
+              fetchPriority="high"
+            />
+          </Link>
+        </div>
 
-    //     <div className={styles.controls}>
-    //       {!useAuth().isAuthenticated && <LoginButton />}
-    //       {useAuth().isAuthenticated && <LogoutButton />}
-    //     </div>
-    //   </div>
-    // </header>
+        <div className={styles.controls}>
+          {!useAuth().isAuthenticated && <LoginButton />}
+          {useAuth().isAuthenticated && <LogoutButton />}
+        </div>
+      </div>
+    </header>
   )
 }
 export default Header
