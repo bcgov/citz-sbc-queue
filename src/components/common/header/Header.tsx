9f473f581ab25a39ba2d2e2bfa70b35e8ef6b7a2
Taylor Friesen <Taylor.Friesen@gov.bc.ca>
2025-12-11 19:52:37 +0000

    update header and footer to use new spacing

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -5,10 +5,10 @@ import { Loginout, SvgBcLogo } from "@/components"
 
 export const Header = () => {
   return (
-    <header className="flex flex-row w-full min-h-[65px] h-[65px] justify-center border-b-(length:--border-width-sm) border-b-border-light px-md">
+    <header className="flex flex-row w-full min-h-[65px] h-[65px] justify-center border-b-sm border-b-border-light px-md">
       {/** Above is bcds-header class */}
       {/** below is bcds-header--container class */}
-      <div className="flex flex-row grow max-w-[1200px] justify-between items-center gap-(--length-padding-md)">
+      <div className="flex flex-row grow max-w-[1200px] justify-between items-center">
         <div className="flex max-h-[65px] h-[65px] max-w-[129px] w-[129px]">
           <Link href="/" title="BC Government home" className="flex w-full h-full">
             <SvgBcLogo />
