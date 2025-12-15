e2ed5bf2cd10f8082ec30f44364e011ad0701f8d
Taylor Friesen <Taylor.Friesen@gov.bc.ca>
2025-12-15 21:29:20 +0000

    set top level gird to change on med and higher displays

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next"
+import type { Metadata, Viewport } from "next"
 import "../styles/globals.css"
 import localFont from "next/font/local"
 import { AuthProvider } from "@/components/auth/AuthProvider"
@@ -38,6 +38,12 @@ export const metadata: Metadata = {
                 offices with a large number of services.`,
 }
 
+// Used to enable responsive designs
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+}
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -47,12 +53,12 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${BCSans.variable} font-sans`}>
         <AuthProvider />
-        <main className="min-h-screen max-h-screen grid grid-cols-12 grid-rows-fr auto-rows-max gap-4">
-          <header className="col-span-full h-auto">
+        <main className="min-h-screen max-h-screen grid md:grid-cols-12 sm:grid-cols-4 grid-rows-fr auto-rows-max gap-4">
+          <header className="col-span-full h-auto sticky top-[0vh]">
             <Header />
             <Navigation />
           </header>
-          <div className="grow col-span-8 col-start-3">{children}</div>
+          <div className="md:col-span-8 md:col-start-3 col-span-full">{children}</div>
           <footer className="sticky top-[100vh] col-span-full h-auto">
             <Footer />
           </footer>
