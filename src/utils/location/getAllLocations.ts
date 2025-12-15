04143411fb87b5f6e74ba772bbce56b750c28b4a
stoews <scott.toews@gov.bc.ca>
2025-12-15 17:34:38 +0000

    update type

--- src/utils/location/getAllLocations.ts
+++ src/utils/location/getAllLocations.ts
@@ -1,9 +1,9 @@
-import type { Location } from "@/app/api/location/types"
+import type { LocationList } from "@/app/api/location/types"
 import { prisma } from "@/lib/prisma"
 
-export async function getAllLocations(): Promise<Location[]> {
+export async function getAllLocations(): Promise<LocationList> {
   const locations = await prisma.location.findMany({
     where: { deletedAt: null },
   })
-  return locations as Location[]
+  return locations as LocationList
 }
