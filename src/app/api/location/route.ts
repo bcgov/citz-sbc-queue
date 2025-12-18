8782a3505d3d0d96d05ec793b310d3b55f20f8ea
stoews <scott.toews@gov.bc.ca>
2025-12-18 18:53:44 +0000

    remove api

--- src/app/api/location/route.ts
+++
@@ -1,4 +0,0 @@
-export { DELETE } from "@/lib/prisma/location/delete"
-export { GET } from "@/lib/prisma/location/get"
-export { POST } from "@/lib/prisma/location/post"
-export { PUT } from "@/lib/prisma/location/put"
