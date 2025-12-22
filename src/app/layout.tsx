bbf6306c1627488d9773ceeacd2d0f281296085d
Taylor Friesen <Taylor.Friesen@gov.bc.ca>
2025-12-22 18:53:10 +0000

    remove unnecessary viewport, fix grid, update header import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata, Viewport } from "next"
 import "../styles/globals.css"
 import localFont from "next/font/local"
 import { AuthProvider } from "@/components/auth/AuthProvider"
-import { Footer, Header, Navigation } from "@/components/common"
+import { Footer, Header } from "@/components/common"
 
 const BCSans = localFont({
   src: [
@@ -38,12 +38,6 @@ export const metadata: Metadata = {
                 offices with a large number of services.`,
 }
 
-// Used to enable responsive designs
-export const viewport: Viewport = {
-  width: "device-width",
-  initialScale: 1,
-}
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -53,10 +47,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${BCSans.variable} font-sans`}>
         <AuthProvider />
-        <main className="min-h-screen max-h-screen grid md:grid-cols-12 sm:grid-cols-4 grid-rows-fr auto-rows-max gap-4">
-          <header className="col-span-full h-auto sticky top-[0vh]">
+        <main className="min-h-screen max-h-screen min-w-screen max-w-screen grid md:grid-cols-12 sm:grid-cols-4 grid-rows-fr auto-rows-max gap-4">
+          <header className="col-span-full max-h-auto md:max-h-[118px] bg-background-default sticky top-[0vh]">
             <Header />
-            <Navigation />
           </header>
           <div className="md:col-span-8 md:col-start-3 col-span-full">{children}</div>
           <footer className="sticky top-[100vh] col-span-full h-auto">
