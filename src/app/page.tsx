8f1c2f18a1ec29770b3b82626d51409af89dbaea
Taylor Friesen <Taylor.Friesen@gov.bc.ca>
2025-12-22 21:01:25 +0000

    reorder tags to better match HTML standards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,12 @@ import { ExampleModal } from "@/components/examples/ExampleModal"
 
 export default function Home() {
   return (
-    <div className="grid p-2 m-2 gap-2 md:grid-cols-8 md:m-8 md:p-8 md:gap-4">
+    <div className="grid p-sm m-sm gap-sm md:grid-cols-8 md:m-xl md:p-lg">
       <div className="md:col-span-6 md:col-start-2">
         <div className="flex flex-col justify-around ">
           <p className="text-xl">TEST</p>
           <div className="self-center text-h4">Some body content here </div>
-          <div className="col-span-8 flex justify-between items-center gap-2 mx-auto">
+          <div className="flex justify-between items-center gap-2 mx-auto">
             <ExampleModal />
             <ExampleDialog />
           </div>
