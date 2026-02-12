5f4e6b18131b64bd3d643509631136e594c713a3
Taylor Friesen <Taylor.Friesen@gov.bc.ca>
2026-02-12 14:52:45 -0800

    update tests for running in container environment

--- playwright.config.ts
+++ playwright.config.ts
@@ -84,10 +84,10 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   /* Comment out if running with Docker container */
-  webServer: {
-    command: 'npm run dev',
-    url: 'http://localhost:3000',
-    reuseExistingServer: !process.env.CI,
-    timeout: 120 * 1000,
-  },
+  // webServer: {
+  //   command: 'npm run dev',
+  //   url: 'http://localhost:3000',
+  //   reuseExistingServer: !process.env.CI,
+  //   timeout: 120 * 1000,
+  // },
 })
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { ExampleModal } from "@/components/examples/ExampleModal"
 export default function Home() {
   return (
     <div className="flex flex-col">
-      <h1 className="text-xl">TEST</h1>
+      <h1 className="">TEST</h1>
       <div className="self-center text-h4">Some body content here </div>
       <div className="flex justify-between items-center gap-2 mx-auto">
         <ExampleModal />
